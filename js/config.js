f41374f9ef9fc8c1c0ee6aaac4fd68801ba14dde
Amarjeet kumar <102892315+Amarjeetkumart@users.noreply.github.com>
2026-02-25 11:55:45 +0530

    Update AWS Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_GgCLHtTQP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6e1r618p37nv6cq2t0ak8g8unt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
